55b4d3de0c1b14efeb60bd0632429035bc292e50
27 <jussivanbm1029@gmail.com>
2024-11-02 13:34:13 -0300

    add piano audios and implement finals js elements

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,39 @@
 const pianoKeys = document.querySelectorAll(".piano_keys .keys");
+let audio = new Audio("src/tunes/a.wav");
+let mapedKeys = [];
+const volumeSlider = document.querySelector(".volume_slider input");
+const keysCheck = document.querySelector(".keys_check input");
+
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
+}
 
 pianoKeys.forEach((key) => {
-    console.log(key);
+    key.addEventListener("click", () => playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
 });
+
+document.addEventListener("keydown" , (e) => {
+    if(mapedKeys.includes(e.key)) {
+        playTune(e.key);
+    }
+});
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+};
+
+volumeSlider.addEventListener("input", handleVolume);
+
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle('hide'));
+}
+
+keysCheck.addEventListener("click", showHideKeys);
